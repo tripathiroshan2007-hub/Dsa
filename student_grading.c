f55c116466ad370a51f45faafc0268a850a7bd1d
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-01-14 19:23:49 +0530

    student_grading.c

---
+++ student_grading.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("enter number (0-100):");
+    scanf("%d",&marks);
+
+    if(marks<30){
+        printf("grade C\n");
+    }
+    else if(marks>=30&& marks<70){
+        printf("grade B\n");
+    }
+else if(marks>=70 &&marks<90){
+printf("grade A\n");
+}
+else{
+    printf("grade A+\n");
+}
+return 0;
+}
