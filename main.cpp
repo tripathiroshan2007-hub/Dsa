3f6656dd169f26f9ad01dc2d63beaeb76c3fa6e1
tripathiroshan2007-hub <tripathiroshan2007@gmail.com>
2026-02-01 19:56:32 +0530

    Correct prime number checking loop
    Fix loop condition in prime number check.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     cout<<"Enter a number: ";
     cin>>n;
 bool isPrime=true;
- for(int i=2; i<=n;i++){
+ for(int i=2; i<=n-1;i++){
      if(n%i==0 && i!=n){
          isPrime=false;
          break;
