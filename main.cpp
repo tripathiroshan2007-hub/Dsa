e9e883648f1f1b926c9dc92f026568bb4610b17f
tripathiroshan2007-hub <tripathiroshan2007@gmail.com>
2026-02-01 20:00:46 +0530

    Optimize prime number checking logic
    Refactor prime number check loop to improve efficiency.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     cout<<"Enter a number: ";
     cin>>n;
 bool isPrime=true;
- for(int i=2; i<=n-1;i++){
+ for(int i=2; i*i<=n;i++){
      if(n%i==0 && i!=n){
          isPrime=false;
          break;
