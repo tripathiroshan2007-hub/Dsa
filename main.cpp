962d06629778c0408e8dd7562b2d6543b6a4ac5d
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-02-01 19:50:22 +0530

    cheching whether  no is prime or not

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-   int n=20;
-   int oddSum=0;
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+bool isPrime=true;
+ for(int i=2; i<=n;i++){
+     if(n%i==0 && i!=n){
+         isPrime=false;
+         break;
+     }
+ }
+   if (isPrime==true){
+       cout<<"prime no\n";
+   } else {
+       cout<<"not prime\n";
+   }
 
-  for(int i=1;i<=n; i++){
-     if (i%2!=0){
-        oddSum+=i;
-     }
-     }
-     cout<<"oddSum="<<oddSum;
-  
 
      
     cout<<endl;
