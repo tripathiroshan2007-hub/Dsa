03f4e69475f49b5ac78d31d683e0e98017d56a05
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-02-02 22:02:02 +0530

    Floyd's Pattern

--- pattern.cpp
+++ pattern.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main() {
 int n=5;
-char ch='A';
-for(int i=0;i<n; i++){
+int count=0;
+for(int i=0;i<n+1; i++){
 
-for(int j=0; j<i+1; j++){
-cout<< ch <<" ";
-
+for(int j=0; j<i; j++){
+cout<< count <<" ";
+count+=1;
 }
-ch++;
+
 cout<<endl;
 }
 return 0;
