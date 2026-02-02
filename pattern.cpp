5cfa7d69943a0d0e6a3377e4c6766f86a11f99f4
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-02-02 22:48:01 +0530

    Pyramid Problem

--- pattern.cpp
+++ pattern.cpp
@@ -5,12 +5,15 @@ int n=5;
 
 for(int i=0;i<n; i++){
 
-for(int j=0; j<i; j++){
+for(int j=0; j<n-i-1; j++){
 cout<<" ";
 
 }
-for(int k=0; k<n-i;k++){
-    cout<<i+1;
+for(int k=1; k<=i+1;k++){
+    cout<<k;
+}
+for(int l=i; l>0; l--){
+    cout<<l;
 }
 
 cout<<endl;
