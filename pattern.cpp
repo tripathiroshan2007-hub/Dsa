610433bcdeb63b0987fc5fa1c5fdfde7638925f6
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-02-08 10:31:18 +0530

    Butterfly Pattern

--- pattern.cpp
+++ pattern.cpp
@@ -4,46 +4,49 @@ int main() {
 int n=4;
 
 for(int i=0;i<n; i++){
-    // spaces
-    for(int j=0; j<n-i-1;j++){
-        cout<< "  ";
+    for(int j=0; j<i+1; j++){
+        cout<<"* ";
     }
-    cout<<"* ";
     
 
-    
-    
-   if(i!=0){
-    // spaces
-    for( int j=0; j<2*i-1; j++){
-        cout<<"  ";
-    }
-
-    cout<<"*";
-   }
-
-
-cout<<endl;
-}
-
-int m=4;
-for(int i=0;i<m-1; i++){
-    // 
-    for(int j=0;j<i+1;j++){
-        cout<<"  ";
-    }
-    cout<<"* ";
-
-      if(i!=m-2){
-        // spaces
-        for(int k=0;k<2*(m-i-2)-1;k++){
+    if(i!=n){
+        for(int k=0; k<2*(n-i)-2;k++){
             cout<<"  ";
         }
-        cout<<"*";
-      }
-      cout<<endl;
+        
+  
+    for( int j=0; j<i+1; j++){
+        cout<<"* ";
+    }
     
-}
+    }
+    cout<<endl;
+    }
+    int m=4;
 
-return 0;
+for(int i=0;i<m; i++){
+    for(int  j=m-i;j>0; j--){
+        cout<<"* ";
+    }
+    
+
+    if(i!=m){
+        for(int k=2; k<2*(m/2+i)-2;k++){
+            cout<<"  ";
+        }
+        
+  
+    for( int j=m-i; j>0; j--){
+        cout<<"* ";
+    }
+    
+    }
+    cout<<endl;
+    
+   
+   
 }
+return 0;
+} 
+
+
