f7fac4c964d04399f8417d1bf29a1845f9e19d87
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-03-02 17:28:27 +0530

    Calculating nCr binomial cofficient of n and r.

--- code.cpp
+++ code.cpp
@@ -2,20 +2,29 @@
 #include<string>
 using namespace std;
 
-int sumofDigits(int num){
-    int digSum=0;
-    while(num>0){
-        int lastDigit=num%10;
-        num/=10;
-        digSum+=lastDigit;
+int factN(int n){
+  int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
     }
-    return digSum;
+    return fact;
 }
-
-
+int factR(int r){
+  int fact=1;
+    for(int i=1;i<=r;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int factNMR(int nMinusR){
+  int fact=1;
+    for(int i=1;i<=nMinusR;i++){
+        fact*=i;
+    }
+    return fact;
+}
 int main(){
-    cout<<"sum="<<sumofDigits(1234)<<endl;
-   
+    int finalResult = factN(4)/(factR(2)*factNMR(4-2));
+    cout<<"finalResult="<<finalResult<<endl;
     return 0;
 }
-    
