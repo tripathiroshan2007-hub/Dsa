fd96e5b7f9b49f5a05799b7fa0b9dc9eb0c3fd64
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-03-03 15:56:37 +0530

    uses of bitwise operators

--- code.cpp
+++ code.cpp
@@ -2,23 +2,15 @@
 #include<string>
 using namespace std;
 
-int binToDec(int binNum){
-int ans=0, pow=1;
 
-while(binNum>0){
-    int rem= binNum%10;
-    ans+= rem * pow;
-
-    binNum/=10;
-    pow*=2;
-}
-return ans;
-}
    
 
 int main(){
-    int binNum=10011;
-
-   cout<<"Decimal number: "<<binToDec(binNum)<<endl;
+   int a=6,b=10;
+   cout<<(a & b)<<endl;
+    cout<<(a | b)<<endl;
+    cout<<(a ^ b)<<endl;
+    cout<<(10 << 2)<<endl;
+    cout<<(10>>1)<<endl;
     return 0;
 }
