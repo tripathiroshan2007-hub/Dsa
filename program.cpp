b496e08fa6e9e3f109e814a1a941d38830138dd7
Roshan Tripathi <tripathiroshan2007@gmail.com>
2026-03-03 17:33:20 +0530

    Array program

--- program.cpp
+++ program.cpp
@@ -5,110 +5,20 @@ using namespace std;
 
   int main(){
     
-    int rollNo;
-    cin>>rollNo;
-
-    do{
-      // if(rollNo==10){
-      //   // break;
-
-      // }
-    
-    switch(rollNo){
-
-    
-      // int myFunction(int rollNo);
-      //   return rollNo;
+    string name[20]={ "Rohit", "Suresh", "Amit", "Rahul", "Vijay", "Anil", "Sunil", "Pawan", "Karan", "Arjun",
+                      "Ravi", "Sanjay", "Manish", "Ashish", "Nikhil", "Sandeep", "Prakash", "Deepak", "Ajay", "Gaurav"};
+                      int rollNo[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+                      for(int i=0;i<20;i++){
+                        if(rollNo[i]==10){
+                          
+                          break;
+                        }
+                        cout<<"Name:"<<name[i]<<endl;
+                          cout<<"RollNo:"<<rollNo[i]<<endl;
+                      }
       
      
-    
-
-          // if(rollNo==10){
-          //   break;
-          // }   
-       case 1:
-       
-      //  myFunction(1);
-       cout<<"Name: Roshan"<<endl;
-       cout<<"Age:20"<<endl;
-       case 2:
-        
-        // myFunction(2);
-       cout<<"Name: Rosh"<<endl;
-       cout<<"Age:20"<<endl;
-       case 3:
-        
-        // myFunction(3);
-       cout<<"Name: aman"<<endl;
-       cout<<"Age:20"<<endl;
-       case 4:
-        // myFunction(4);
-       cout<<"Name: pawan"<<endl;
-       cout<<"Age:20"<<endl;
-       case 6:
-        // myFunction(6);
-       cout<<"Name: rajkumar"<<endl;
-       cout<<"Age:20"<<endl;
-       case 7:
-        // myFunction(7);
-       cout<<"Name: akash"<<endl;
-       cout<<"Age:20"<<endl;
-       case 8:
-        // myFunction(8);
-       cout<<"Name: shubham"<<endl;
-       cout<<"Age:20"<<endl;
-       case 9:
-        // myFunction(9);
-       cout<<"Name: kishan"<<endl;
-       cout<<"Age:20"<<endl;
-       case 10:
-        // myFunction(10);
-       cout<<"Name: adarsh"<<endl;
-       cout<<"Age:20"<<endl;
-       case 11:
-        // myFunction(11);
-       cout<<"Name: utpal"<<endl;
-       cout<<"Age:20"<<endl;
-       case 12:
-        // myFunction(12);
-       cout<<"Name: akhil"<<endl;
-       cout<<"Age:20"<<endl;
-       case 13:
-        // myFunction(13);
-       cout<<"Name: nageshwar"<<endl;
-       cout<<"Age:20"<<endl;
-
-       case 14: 
-      //  myFunction(14);
-       cout<<"Name: shakshm"<<endl;
-       cout<<"Age:20"<<endl;
-       case 15:
-        // myFunction(15);
-       cout<<"Name: shivam"<<endl;
-       cout<<"Age:20"<<endl;
-       case 16:
-        // myFunction(16);
-       cout<<"Name: alok"<<endl;
-       cout<<"Age:20"<<endl;
-       case 17:
-        // myFunction(17);
-       cout<<"Name: aryan"<<endl;
-       cout<<"Age:20"<<endl;
-       case 18:
-        // myFunction(18);
-       cout<<"Name: deepa mam"<<endl;
-       cout<<"Age:20"<<endl;
-       case 19:
-        // myFunction(19);
-       cout<<"Name: yogesh sir"<<endl;
-       cout<<"Age:20"<<endl;
-       case 20:
-        // myFunction(20);
-       cout<<"Name: anjali mam"<<endl;
-       cout<<"Age:20"<<endl;
-
-    }
-  } while(rollNo>=1 && rollNo<=20);   
+      
     return 0;
     }
 
